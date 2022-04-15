878824b9a5218d505d979abca394c63c7455026f
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-16 00:06:09 +0300

    ft:add a quote-delete functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quote!:Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
