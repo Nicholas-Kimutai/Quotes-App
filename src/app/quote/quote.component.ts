6dc441d4d941ab5f4d99b13e86fd61d29fb28420
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-15 22:25:26 +0300

    create quotes array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote[]=[
+    new Quote("Nick","Steve Jobs","You can’t connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.",490,4,new Date(2020,1,12)),
+    new Quote("Nick","Henry David Thoreau","Go confidently in the direction of your dreams! Live the life you've imagined.",309,5,new Date(2021,6,17)),
+    new Quote("Nick","Nelson Mandela","The greatest glory in living lies not in never falling, but in rising every time we fall.",353,10, new Date(2012,12,12))
+  ];
 
   constructor() { }
 
